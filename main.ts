65bd8fa1eec3a42c477d4b9da96e01e5c16c4d5a
gabywaby <123405090+gabywaby@users.noreply.github.com>
2023-01-23 10:03:57 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+basic.forever(function () {
+    basic.showString("wake up")
+    basic.showString("eat food")
+    basic.showString("brush teeth")
+    basic.showString("get all thing ready for school and put them in back pack")
+})
